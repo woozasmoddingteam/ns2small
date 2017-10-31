03b4fb0971b7521acdfd058a75609b0cce6ae474
Las <las@protonmail.ch>
2017-10-31 11:10:47 +0100

    Make doors and tech points full-size

--- lua/ns2small/FileHooks.lua
+++ lua/ns2small/FileHooks.lua
@@ -4,3 +4,5 @@ ModLoader.SetupFileHook("lua/Mixins/BaseModelMixin.lua",    "lua/ns2small/BaseMo
 ModLoader.SetupFileHook("lua/Player.lua",                   "lua/ns2small/Player.lua",            "post")
 ModLoader.SetupFileHook("lua/ExtentsMixin.lua",             "lua/ns2small/ExtentsMixin.lua",      "post")
 ModLoader.SetupFileHook("lua/WallMovementMixin.lua",        "lua/ns2small/WallMovementMixin.lua", "post")
+ModLoader.SetupFileHook("lua/TechPoint.lua",                "lua/ns2small/TechPoint.lua",         "post")
+ModLoader.SetupFileHook("lua/Door.lua",                     "lua/ns2small/Door.lua",              "post")
